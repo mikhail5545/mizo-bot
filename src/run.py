0f84d6cbe269b78eb7cf15fd9ff7c76b8bfc7494
Mikhail Kulik <67920426+mikhail5545@users.noreply.github.com>
2024-08-29 15:32:51 +0300

    Add files via upload

--- src/run.py
+++ src/run.py
@@ -1,1 +1,38 @@
+import asyncio
+import sqlite3
+import logging
+from load_dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from sys import stdout
+from os import getenv
+
+from core.handle import dp, handle_events
+
+
+
+
+
+async def __run() -> None:
+
+    load_dotenv()
+    TOKEN = getenv("TOKEN")
+
+    bot = Bot(token=TOKEN)
+
+    await handle_events(bot)
+    await dp.start_polling(bot)
+    
+
+
+if __name__ == "__main__":
+
+    con = sqlite3.connect('core/database/orders.db')
+    cur = con.cursor()
+    cur.execute("CREATE TABLE if not exists orders(id, user, userID, category, url, price, delivery_type, address, size, label, style, status, color, date)")
+    con.commit()
+    con.close()
+
+    logging.basicConfig(level=logging.INFO, stream=stdout)
+    asyncio.run(__run())
+
 
